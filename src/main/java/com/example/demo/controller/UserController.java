52a8acbff0b8a24a6784d2da508be661c39dfa71
kshinlee <kshinlee@naver.com>
2024-11-09 21:51:55 +0900

    ddd

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class UserController {
 
-    // GET 방식으로 사번을 반환하는 APIssss
+    // GET 방식으로 사번을 반환하는 APIssssxss
     @GetMapping("/user")
     public String getUserId() {
         // employeeId를 경로에서 추출해 사번을 리턴
